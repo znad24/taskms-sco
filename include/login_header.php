d4edca94cff7252f835b8a5cd9f699a2accefe9a
Dani Zudiansyah (ICT) <dani.zudiansyah@hokahokabento.local>
2023-10-26 09:17:36 +0700

    update header TMS

--- include/login_header.php
+++ include/login_header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Employee Task Management</title>
+  <title>Task Management System</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
