719192eda27d9f84b95dcdd281b1250b8ada5621
znad24 <dani.zudiansyah@gmail.com>
2026-02-06 18:14:51 +0000

    update kembalikan ke awal  koneksi.php

--- inc/koneksi.php
+++ inc/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = getenv('DB_HOST') ?;   // NAMA CONTAINER menggunakan variabel yang sudah di seting di dalam docker-compose.yml
-$user = getenv('DB_USER') ?:root;
-$pass = getenv('DB_PASS') ?:root123;
-$db = getenv('DB_NAME') ?:etms_db;
+$user = getenv('DB_USER') ?;
+$pass = getenv('DB_PASS') ?;
+$db = getenv('DB_NAME') ?;
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
